8919a96bf27b50bfe080598ee0af10a2b55debf8
Sune <fengel@gmail.com>
2017-04-25 18:37:08 +0200

    try send file

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, '/build')))
 
-app.get('*', (req, res, next) => {
-    next()
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/build/index.html'))
 })
 
 const port = process.env.PORT || 3000
